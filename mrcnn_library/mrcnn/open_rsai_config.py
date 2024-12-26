26ccbe62a6632d86cb9c61303e84c3225109affa
Sergey Miroshnichenko <s.miroshnichenko@devar.org>
2024-12-26 17:14:14 +0300

    OpenRSAI custom detector configs

---
+++ mrcnn_library/mrcnn/open_rsai_config.py
@@ -0,0 +1,50 @@
+import mrcnn.config
+
+GREEN_CLASS_NAMES = ['BG', 'green']
+
+class GreeneryConfig(Config):
+    NAME = "green"
+    IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+    NUM_CLASSES = len(GREEN_CLASS_NAMES)
+    STEPS_PER_EPOCH = 600
+    VALIDATION_STEPS = 20
+
+    LEARNING_RATE = 0.001
+
+    BACKBONE = "resnet101"
+
+    IMAGE_MIN_DIM = 1024
+    IMAGE_MAX_DIM = 1024
+
+    RPN_NMS_THRESHOLD = 0.65
+    RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
+    DETECTION_MIN_CONFIDENCE = 0.7
+    DETECTION_NMS_THRESHOLD = 0.3
+
+HYDRO_CLASS_NAMES = ['BG', 'hydro']
+
+class HydroConfig(Config):
+    NAME = "hydro"
+
+    IMAGES_PER_GPU = 1
+
+    GPU_COUNT = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = len(HYDRO_CLASS_NAMES)
+
+    STEPS_PER_EPOCH = 200
+    VALIDATION_STEPS = 20
+
+    LEARNING_RATE = 0.001
+
+    BACKBONE = "resnet101"
+
+    IMAGE_MIN_DIM = 1024
+    IMAGE_MAX_DIM = 1024
+
+    RPN_NMS_THRESHOLD = 0.7
+    RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
+    DETECTION_MIN_CONFIDENCE = 0.7
+    DETECTION_NMS_THRESHOLD = 0.3
