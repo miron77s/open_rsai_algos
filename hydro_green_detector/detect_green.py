0a8ed8342f0d8850c7b4f34c624480f9d9a82c3b
Sergey Miroshnichenko <s.miroshnichenko@devar.org>
2024-12-27 13:23:29 +0300

    Config param fix

--- hydro_green_detector/detect_green.py
+++ hydro_green_detector/detect_green.py
@@ -6,4 +6,6 @@ if __name__ == "__main__":
 
     if len(argv) != 5:
         raise Exception("\nUsage:\n" + f"\t{argv[0]} [output-path] [weights-file] [work-region-shp] [path-to-raster]")
-    mrcnn.open_rsai_detect.greenery(argv[1], argv[2], argv[3], argv[4], GreeneryDetectConfig())
+
+    config = GreeneryDetectConfig()
+    mrcnn.open_rsai_detect.greenery(argv[1], argv[2], argv[3], argv[4], config)
