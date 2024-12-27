dd98e49d240911d16a3099f84a5b1ea7abeb3c53
Sergey Miroshnichenko <s.miroshnichenko@devar.org>
2024-12-27 19:42:46 +0300

    Moved fine-tuned train to library

--- hydro_green_train/train_green.py
+++ hydro_green_train/train_green.py
@@ -12,6 +12,7 @@ from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
 from mrcnn.open_rsai_dataset import CocoDataset, DEFAULT_DATASET_YEAR
+import mrcnn.open_rsai_train
 
 ############################################################
 #  Configurations
@@ -103,29 +104,4 @@ if __name__ == '__main__':
     # augmentation = imgaug.augmenters.Fliplr(0.5)
 
     # *** This training schedule is an example. Update to your needs ***
-
-    # Training - Stage 1
-    print("Training network heads")
-    model.train(dataset_train, dataset_val,
-        learning_rate=config.LEARNING_RATE,
-        epochs=40,
-        layers='heads'
-    )
-
-    # Training - Stage 2
-    # Finetune layers from ResNet stage 4 and up
-    print("Fine tune Resnet stage 4 and up")
-    model.train(dataset_train, dataset_val,
-        learning_rate=config.LEARNING_RATE,
-        epochs=120,
-        layers='4+'
-    )
-
-    # Training - Stage 3
-    # Fine tune all layers
-    print("Fine tune all layers")
-    model.train(dataset_train, dataset_val,
-        learning_rate=config.LEARNING_RATE / 10,
-        epochs=150,
-        layers='all'
-    )
+    def mrcnn.open_rsai_train.fine_tuned ( model, dataset_train, dataset_val, config )
